bfa12446a5716288b115867490ac1c0423365ae3
x0x1x0 <janlucas.sternberg@gmail.com>
2024-02-17 14:24:04 +0100

    vue comparison to vanilla

---
+++ 01-starting-project/app.js
@@ -0,0 +1,31 @@
+
+
+
+Vue.createApp({
+ data() {
+        return{
+            goals:[],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
+
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li')
+//     listItemEl.textContent = enteredValue;
+//     listEl.append(listItemEl);
+//     inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal )
